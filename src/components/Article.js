99db155e42f00e7c266546ea782af7a4bb89faba
nmogoko <+>
2024-08-07 20:42:22 +0100

    successfully passed labs

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-function Article({ post: { title, date, preview, minutes }, key }) {
+function Article({ title, date, preview}) {
   return (
     <article>
       <h3>{title}</h3>
       <small>{date ?? "January 1, 1970"}</small>
       <p>{preview}</p>
-      {minutes > 30 ? (
+      {/* {minutes > 30 ? (
         <span>{`${Array(Math.ceil(minutes / 10))
           .fill("🍱")
-        } ${minutes} min read`}</span>
+          .join(" ")} ${minutes} min read`}</span>
       ) : (
-        <span>{`${Array(Math.ceil(minutes / 5)).fill(
-          "☕️"
-        )} ${minutes} min read`}</span>
-      )}
+        <span>{`${Array(Math.ceil(minutes / 5))
+          .fill("☕️")
+          .join(" ")} ${minutes} min read`}</span>
+      )} */}
     </article>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import Article from './Article'
+import React from "react";
+import Article from "./Article";
 
 function ArticleList(props) {
   return (
     <main>
-        {props.posts.map((post, key) => <Article key={key} post={post}/> )}
+      {props.posts?.map((post) => (
+        <Article
+          key={post.id}
+          title={post.title}
+          date={post.date}
+          preview={post.preview}
+        />
+      ))}
     </main>
-
-  )
+  );
 }
 
-export default ArticleList
+export default ArticleList;
